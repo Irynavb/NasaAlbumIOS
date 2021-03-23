6bbe4c7ba7de693f57f6fd657e29ee524c2d1a26
Irynavb <irynabetancourt@gmail.com>
2021-03-22 22:33:02 -0400

    minor refactoring

--- NasaAlbumIOS/Controllers/SingleImageViewController.swift
+++ NasaAlbumIOS/Controllers/SingleImageViewController.swift
@@ -123,3 +123,22 @@ final class SingleImageViewController: UIViewController {
         dismiss(animated: true)
     }
 }
+
+extension  SingleImageViewController {
+
+    private func getImageTitle() -> String {
+        item.data[0].title ?? "Unknown Image Title"
+    }
+
+    private func getImageLocation() -> String {
+        "Location: \(item.data[0].location ?? "Unknown")"
+    }
+
+    private func getImagePhotographer() -> String {
+        "Photographer: \(item.data[0].photographer ?? "Unknown")"
+    }
+
+    private func getImageDescription() -> String {
+        item.data[0].datumDescription ?? ""
+    }
+}
